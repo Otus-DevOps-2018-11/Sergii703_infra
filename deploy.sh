a2dafc5c4d9518854db58879be36d353aa91629e
Sergii703 <sergey703@gmail.com>
2019-01-19 13:56:46 +0100

    make file executable

--- deploy.sh
+++ deploy.sh
@@ -2,7 +2,7 @@
 # copy repo with application
 git clone -b monolith https://github.com/express42/reddit.git
 # move to specyfic folder and install dependencies
-cd redit && bundle install
+cd reddit && bundle install
 # start server and checking port
 puma -d
 ps aux | grep puma

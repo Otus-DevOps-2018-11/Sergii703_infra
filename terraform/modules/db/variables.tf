6cdfdf4777061da7a3eebe569ee5ff7b6847cca2
Sergii703 <sergey703@gmail.com>
2019-01-25 22:24:47 +0100

    added new modules

--- terraform/modules/db/variables.tf
+++ terraform/modules/db/variables.tf
@@ -7,7 +7,7 @@ variable zone {
 }
 
 variable db_disk_image{
-        description = "Disk image for reddit app"
+        description = "Disk image for reddit db"
         default = "reddit-db-base"
 }
 
